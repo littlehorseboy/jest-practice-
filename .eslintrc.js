cc18f2dd4df64ff5823a442f960827914c3637cc
horse <littlehorseboy@gmail.com>
2019-03-12 17:42:07 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  // parser: 'babel-eslint',
+  // parserOptions: {
+  //   ecmaVersion: 2017,
+  //   sourceType: 'module',
+  // },
+  // env: {
+  //   browser: true,
+  //   es6: true,
+  // },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    jest: 'readonly',
+    test: 'readonly',
+    expect: 'readonly',
+    beforeEach: 'readonly',
+    afterEach: 'readonly',
+    beforeAll: 'readonly',
+    afterAll: 'readonly',
+    describe: 'readonly',
+  },
+  rules: {
+    'linebreak-style': [
+      'error',
+      'windows',
+    ],
+  },
+};
